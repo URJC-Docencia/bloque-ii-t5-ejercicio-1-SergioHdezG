68f1c3ce67cd5f3d247166229ea26295ca20689b
sergio.hernandez <sergio.hernandez@urjc.es>
2023-11-28 12:45:27 +0100

    Código día 28/11/2023

--- src/material/graphs/ELGraph.java
+++ src/material/graphs/ELGraph.java
@@ -1,8 +1,7 @@
 
 package material.graphs;
 
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 /**
  *
@@ -12,29 +11,79 @@ import java.util.List;
  */
 public class ELGraph<V,E> implements Graph<V,E> {
 
+    private Set<ELVertex<V>> vertexSet;
+    private Set<ELEdge<V, E>> edgeSet;
+
+    public ELGraph(){
+        vertexSet = new HashSet<>();
+        edgeSet = new HashSet<>();
+    }
+    
     @Override
     public Collection<? extends Vertex<V>> vertices() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return Collections.unmodifiableCollection(vertexSet);
     }
 
     @Override
     public Collection<? extends Edge<E>> edges() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return Collections.unmodifiableCollection(edgeSet);
     }
 
+    private ELVertex<V> checkVertex(Vertex<V> vertex){
+        if (vertex instanceof ELVertex<V>){
+            ELVertex<V> v = (ELVertex<V>) vertex;
+            if (v.getGraph().equals(this)){
+                return v;
+            }
+        }
+        throw new RuntimeException("The vertex is not in the graph");
+    }
+
+    private ELEdge<V, E> checkEdge(Edge<E> edge){
+        if (edge instanceof ELEdge<V, E>){
+            ELEdge<V, E> v = (ELEdge<V, E>) edge;
+            if (v.getGraph().equals(this)){
+                return v;
+            }
+        }
+        throw new RuntimeException("The edge is not in the graph");
+    }
+    
     @Override
     public Collection<? extends Edge<E>> incidentEdges(Vertex<V> v) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ELVertex<V> vertex = checkVertex(v);
+        Set<Edge<E>> edgeList = new HashSet<>();
+        
+        for (ELEdge<V, E> edge: this.edgeSet){
+            if(vertex.equals(edge.getStarVertex()) || vertex.equals(edge.getEndVertex())){
+                edgeList.add(edge);
+            }
+        }
+        return edgeList;
     }
 
     @Override
     public Vertex<V> opposite(Vertex<V> v, Edge<E> e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ELVertex<V> vertex = checkVertex(v);
+        ELEdge<V, E> edge = checkEdge(e);
+        
+        if (edge.getStarVertex().equals(vertex)){
+            return edge.getEndVertex();
+        }
+        else{
+            return edge.getStarVertex();
+        }
     }
 
     @Override
     public List<Vertex<V>> endVertices(Edge<E> edge) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ELEdge<V, E> e = checkEdge(edge);
+        List<Vertex<V>> vList = new ArrayList();
+        
+        vList.add(e.getStarVertex());
+        vList.add(e.getEndVertex());
+        
+        return vList;
     }
 
     @Override
@@ -71,5 +120,71 @@ public class ELGraph<V,E> implements Graph<V,E> {
     public E removeEdge(Edge<E> edge) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+}
+
+class ELVertex<V> implements Vertex<V>{
+
+    private V element;
+    private final Graph graph;
+
+
+    public ELVertex(V element, Graph graph){
+        this.element = element;
+        this.graph = graph;
+    }
+
+    public void setElement(V element){
+        this.element = element;
+    }
+    @Override
+    public V getElement() {
+        return null;
+    }
+
+    public Graph getGraph() {
+        return graph;
+    }
+}
+
+class ELEdge<V, E> implements Edge<E>{
+    private E element;
+    private ELVertex<V> starVertex;
+    private ELVertex<V> endVertex;
+
+    private final Graph graph;
+
+    public ELEdge(E e, ELVertex<V> starV, ELVertex<V> endV, Graph g){
+        this.element = e;
+        this.starVertex = starV;
+        this.endVertex = endV;
+        this.graph = g;
+    }
+    @Override
+    public E getElement() {
+        return null;
+    }
+
+    public void setElement(E element) {
+        this.element = element;
+    }
+
+    public ELVertex<V> getStarVertex() {
+        return starVertex;
+    }
+
+    public void setStarVertex(ELVertex<V> starVertex) {
+        this.starVertex = starVertex;
+    }
+
+    public ELVertex<V> getEndVertex() {
+        return endVertex;
+    }
+
+    public void setEndVertex(ELVertex<V> endVertex) {
+        this.endVertex = endVertex;
+    }
+
+    public Graph getGraph() {
+        return graph;
+    }
 }
